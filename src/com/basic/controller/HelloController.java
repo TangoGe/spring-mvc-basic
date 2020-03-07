22856b54bbb5c8ec6e284eb4157b65684be02662
G Tang <897015336@qq.com>
2020-03-07 11:18:16 +0800

    注解方式配置spring-mvc

--- src/com/basic/controller/HelloController.java
+++ src/com/basic/controller/HelloController.java
@@ -3,26 +3,23 @@ package com.basic.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 非注解方式需要实现Controller接口
+ * 注解方式配置Controller
  * 
  * @author Tango
  *
  */
-public class HelloController implements Controller {
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// ModelAndView对象
-		ModelAndView modelAndView = new ModelAndView();
-		// 向模型对象中添加一个msg字符串对象
-		modelAndView.addObject("msg", "第一个最基本的spring mvc程序");
-		// 设置返回的视图路径
-		modelAndView.setViewName("/WEB-INF/jsp/welcome.jsp");
-		return modelAndView;
+@Controller
+@RequestMapping(value = "/hello")
+public class HelloController {
+	@RequestMapping(value = "/index")
+	public String handlerRequest(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws Exception {
+		model.addAttribute("msg", "第一个spring mvc 程序");
+		return "welcome";
 	}
-
 }
